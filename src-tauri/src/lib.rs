dea15c37ebb1ec5d080b1785ddafd1cda268ab23
ahaoboy <504595380@qq.com>
2026-01-24 10:15:18 +0800

    desktop updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,7 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init());
 
-    #[cfg(target_os = "windows")]
+    #[cfg(not(target_os = "android"))]
     let builder = builder
         .plugin(tauri_plugin_media::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
